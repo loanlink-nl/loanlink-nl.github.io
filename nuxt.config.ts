a2b369363e93b99174c00c2fb806f3ea89ecbe24
Max Stevens <maxstevens2708@gmail.com>
2025-05-16 14:57:53 +0200

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from "nuxt/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -17,4 +18,8 @@ export default defineNuxtConfig({
 	},
 	compatibilityDate: "2024-04-03",
 	ssr: true,
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	css: ["./assets/css/main.css"],
 });
